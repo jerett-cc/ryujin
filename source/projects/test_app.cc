f8346568319ae04f96dbbb0a396830c7f88ada09
Jerett Cherry <jerett.cherry@colostate.edu>
2024-05-22 12:17:49 -0600

    remove unused code in test_app
    As of this commit, this executable calls .init() only to test the
    interpolation functionality.

--- source/projects/test_app.cc
+++ source/projects/test_app.cc
@@ -54,9 +54,5 @@ int main(int argc, char* argv[]){
 
   braid_Vector *v = new(braid_Vector);
   app.Init(2.5, v);
-
-  std::string fname = "./test_init_at_" + std::to_string(2.5);
-  mgrit::MyVector *v_ = (mgrit::MyVector *)v;
-  app.print_solution(v_->U, 2.5, app.finest_level, fname, false, -1);
-  delete v;
+  
 }
