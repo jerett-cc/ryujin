163f23834248209e09aa2ed80745c26a4e5c6644
Jerett Cherry <jerett.cherry@colostate.edu>
2024-08-20 15:30:41 -0600

    bug fix where number of parameters was OK but an assert triggered

--- source/projects/cpp_myapp_testing.cc
+++ source/projects/cpp_myapp_testing.cc
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
   MPI_Comm comm_world = MPI_COMM_WORLD;//create MPI_object
   //set up app and all underlying data, initialize parameters
   //parse command line parameters, order should be file name, parameter file, px, then the mg hierarcy, i.e. list of refinement levels.
-  Assert(argc > 1/*program*/ + 1/*parameter file*/ + 1/*px*/ + 1/*at least one level refinement*/,
+  Assert(argc >= 1/*program*/ + 1/*parameter file*/ + 1/*px*/ + 1/*at least one level refinement*/,
          dealii::ExcMessage("You must provide the program with a parameter file, a number of spatial processors, "
               "and a multigrid hierarcy. Here, the number of additional parameters needed is at least:" + std::to_string(4-argc)));
   const std::string prm_name(argv[1]);// prm file
