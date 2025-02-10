ab5cb3a0328cbec08c0ca384afd8ceaf906b4abe
Jerett Cherry <jerett.cherry@colostate.edu>
2025-02-10 12:43:37 -0700

    add some files run.sh and startup.cc for iteration 0 of mgrit
    configure the cmake to symlink run.sh to the build/run directory also.

---
+++ source/projects/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# the .prm is $1
+# the nWorld is $2
+# the nX is $3
+# the rest are the refinement levels.
+## warning, these need to be sorted into ascending order eg. 1 3 6 levels of refinement
+## since the logic of this file assumes that $4 is the coarsest refinement.
+
+#TODO: make some kind of assertion here?
+
+prm="$1"
+nworld="$2"
+nx="$3"
+s_refinements="${@:4:($#)}"
+refinements=(${s_refinements})
+
+echo "${prm}"
+echo "${nworld}"
+echo "${nx}"
+echo ${s_refinements}
+echo ${refinements[0]}
+
+mpirun -n "${nworld}" startup "${prm}" "${refinements[0]}" >> setup.log
+
+# once the setupfiles are written, we start the regular program.
+
---
+++ source/projects/startup.cc
@@ -0,0 +1,38 @@
+#include <my_app.h>
+#include <deal.II/base/mpi.h>
+
+#include "euler/description.h"
+
+#include <string>
+
+int main(int argc, char* argv[])
+{
+  // TODO: make this a parameter file option or a cmd line option.
+  using Description = ryujin::Euler::Description;
+  //scoped MPI object, no need to call finalize at the end.
+  dealii::Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);  //create objects
+  MPI_Comm comm_world = MPI_COMM_WORLD;//create MPI_object
+  //set up app and all underlying data, initialize parameters
+  //parse command line parameters, order should be file name, parameter file, px, then the mg hierarcy, i.e. list of refinement levels.
+  Assert(argc >= 1/*program*/ + 1/*parameter file*/ + 1/*minimum refinement*/,
+         dealii::ExcMessage("You must provide the startup program with a parameter file and a"
+			    " mesh refinement. "
+			    "Here, the number of additional parameters needed is at least:"
+			    + std::to_string(3-argc)));
+  const std::string prm_name(argv[1]);// prm file
+  const int refinement = std::stoi(argv[2]);
+
+  std::cout << "prm: " << prm_name << " refinement: " << refinement << std::endl;
+  
+  // // First iteration of MGRIT, initializes the cpoints
+  // mgrit::MyApp<NUMBER, Description, 2> app_0(comm_world, comm_world, {refinement});
+  // app_0.initialize(prm_name);
+  // app_0.write_coarse_points("./initial_coarse/"+app_0.base_name);
+
+  // std::vector<NUMBER>c_points =  app.c_points();
+  // // Print out the vector
+  // for (auto n : c_points)
+  //       std::cout << n << ' ';
+  //   std::cout << '\n';
+
+}
