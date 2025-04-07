28fbe7940efd27f7bc226f585e22718fcb583662
Jerett Cherry <jerett.cherry@colostate.edu>
2025-04-07 16:52:16 -0600

    add two WIP tests, one for does_E_exceed and one for the projection operation

---
+++ source/projects/project_to_reasonable_bounds.cc
@@ -0,0 +1,130 @@
+#include "discretization.h"
+#include "level_structures.h"//for all the objects that are needed for a run.
+#include "time_loop.h"
+#include <deal.II/base/mpi.h>
+#include "euler/description.h"
+#include "euler/hyperbolic_system.h"
+#include "my_app.h"
+#include "state_vector.h"
+#include "mgrit_functions.template.h"
+
+
+
+/**
+ * Right now, this executable runs a simulation equivalent to a ryujin run.
+*/
+using StateVector = mgrit::MyApp<double, ryujin::Euler::Description, 2>::StateVector;
+using App = mgrit::MyApp<double, ryujin::Euler::Description, 2>;
+
+constexpr int dim = 2;
+
+void set_E_and_rho_at_single_point(const double new_point_E, const double new_rho, StateVector &U)
+{
+  // This test only runs on a single process, so all the data structures should
+  // be consistent every time. We will set E on dof=0 to be new_point_E.
+  // and then be done.
+  auto state = std::get<0>(U).get_tensor((unsigned int)0);
+  state[0] = new_rho;
+  state[dim+1] = new_point_E;
+  std::get<0>(U).write_tensor(state, 0);
+}
+
+int main(int argc, char *argv[]){
+
+  const std::string prm_name = argv[1];
+  const std::vector<int> refinement_levels = {0};
+
+  dealii::Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);  //create objects
+  const MPI_Comm comm_world = MPI_COMM_WORLD;
+  
+  mgrit::MyApp<double, ryujin::Euler::Description, 2> app(comm_world, comm_world, refinement_levels);
+
+  app.initialize(prm_name);
+
+  // Set up data.
+  mgrit::MyVector<double, ryujin::Euler::Description, 2> my_U_ok, my_U, my_U_copy, my_U_ref;
+
+  // Initialize data my_U at t = 0.
+  ryujin::Vectors::reinit_state_vector<ryujin::Euler::Description>(my_U.U,
+								   *(app.levels[0]->offline_data));
+  std::get<0>(my_U.U) = app.levels[0]->initial_values->get().interpolate_hyperbolic_vector(0.0);
+  my_U_copy.U = my_U.U;
+  // Initialize my_U_ok and my_U_ref
+  ryujin::Vectors::reinit_state_vector<ryujin::Euler::Description>(my_U_ok.U,
+								   *(app.levels[0]->offline_data));
+  std::get<0>(my_U_ok.U) = app.levels[0]->initial_values->get().interpolate_hyperbolic_vector(0.0);
+  ryujin::Vectors::reinit_state_vector<ryujin::Euler::Description>(my_U_ref.U,
+								   *(app.levels[0]->offline_data));
+  std::get<0>(my_U_ref.U) = app.levels[0]->initial_values->get().interpolate_hyperbolic_vector(0.0);
+
+  const double E_threshold = 900.;
+  
+  // set one of the points to have too large of an internal Energy and a negative density
+  // and check that we see this reflected in the output of does_E_exceed_threshold.
+  set_E_and_rho_at_single_point(1000.,-2. , my_U.U);
+  // At this point, we should have a problem E, and we ought to visualize it.
+  if (mgrit_functions::does_E_exceed_threshold<ryujin::Euler::Description,dim,double>(my_U,
+										      app,
+										      0,
+										      0,
+										      0,
+										      0,
+										      E_threshold,
+										      true))
+  {
+    std::cout << "Did Exceed, see above for where." << std::endl;
+  } else {
+    std::cout << "Problem, E should have exceeded at a point." << std::endl;
+  }
+
+  // Now, we project to stable manifold defined by positive density, and not too large E.
+  mgrit_functions::enforce_physicality_bounds(my_U,0,app,0.);
+
+  // Afterwards, we should be not exceeding the threshold anymore, and we nee to visualize the output.
+  if (mgrit_functions::does_E_exceed_threshold<ryujin::Euler::Description,dim,double>(my_U,
+										      app,
+										      0,
+										      0,
+										      0,
+										      0,
+										      E_threshold,
+										      true))
+  {
+    std::cout << "Did Exceed, see above for where." << std::endl;
+    app.print_solution(my_U.U, 0., 0, "./E_exceeds_afterprojection", 0);
+  } else {
+    std::cout << "Projection has lowered E." << std::endl;
+    app.print_solution(my_U.U, 0., 0, "./E_lowered_afterprojection", 0);
+  }
+
+  // we also need to check that the projection is the identity when acting on
+  // a vector that is already ok. And that we really have different vector after
+  // modifying it and doing a projection.
+
+  const double ref_size = std::get<0>(my_U_ref.U).l1_norm();
+  app.print_solution(my_U_ok.U, 0., 0, "./U_ref", 0);
+  const double modified_size = std::get<0>(my_U.U).l1_norm();
+  const double copy_size = std::get<0>(my_U_copy.U).l1_norm();
+  app.print_solution(my_U_ok.U, 0., 0, "./OK_U_before_projection", 0);
+  // project an OK vector, expect that we have the same norm as the ref.
+  mgrit_functions::enforce_physicality_bounds(my_U_ok,0,app,0.);
+  app.print_solution(my_U_ok.U, 0., 0, "./OK_U_after_projection", 0);
+  const double ok_size = std::get<0>(my_U_ok.U).l1_norm();
+  std::get<0>(my_U_ok.U) -= std::get<0>(my_U_ref.U);
+  app.print_solution(my_U_ok.U, 0., 0, "./OK_U__diff_after_projection", 0);
+  const double diff_size = std::get<0>(my_U_ok.U).l1_norm();
+  if ( diff_size >= 1e-5)
+  {
+    std::cout << "Projection operation not identity on physical vector." << std::endl;
+  } else {
+    std::cout << "Projection operation is identity on physical vector." << std::endl;
+  }
+
+  std::cout << "my_Uafter_projection - my_Ucopy=" << std::setprecision(13)
+	    << modified_size - copy_size << std::endl;
+  std::cout << "my_U_ok_after_projection - my_U_ref=" << std::setprecision(13)
+	    << ok_size - ref_size << std::endl;
+
+  
+  return 0;
+}
