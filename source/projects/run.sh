ebf21c544ecb93abade91721967b7156c08c9797
Jerett Cherry <jerett.cherry@colostate.edu>
2025-10-27 11:49:02 -0600

    use more processes for smaller meshes as a default on startup

--- source/projects/run.sh
+++ source/projects/run.sh
@@ -24,9 +24,9 @@ echo "NX:${nx}"
 echo "NTHREADS:${nthreads}"
 echo "refinements:"${s_refinements}
 
-if [ ${refinements[0]} -lt 4 ]; then
-    echo "Using 1 process for startup since the # refinements on coarsest level is small."
-    DEAL_II_NUM_THREADS="${nthreads}" mpirun -n 1 startup "${prm}" ${s_refinements}
+if [ ${refinements[0]} -lt 6 ]; then
+    echo "Using 2 process for startup since the # refinements on coarsest level is small."
+    DEAL_II_NUM_THREADS="${nthreads}" mpirun -n 2 startup "${prm}" ${s_refinements}
 else
     DEAL_II_NUM_THREADS="${nthreads}" mpirun -n "${nworld}" startup "${prm}" ${s_refinements}
 fi
